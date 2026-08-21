626ffbd5d8e3af98ffadfe30712fd8fc826b0be4
JuyiAfsh <hamidajannat25@gmail.com>
2026-08-21 19:37:51 +0200

    Python first program

---
+++ prints_and_comments.py
@@ -0,0 +1,11 @@
+# Printing a greeting message 
+print("Hello, world!")
+
+# Printing my name 
+print("My name is Juyairiya.")
+
+# Printing a fun fact about myself
+print("I like skipping.")
+
+# Performing and printing a simple calculation 
+print("The result of 3 + 5 is:" , 3 / 5)
