b31757d48264275ba6685eeae2037c932c39bef0
JuyiAfsh <hamidajannat25@gmail.com>
2026-08-22 19:39:21 +0200

    Getting started with programming

---
+++ keywords.py
@@ -0,0 +1,6 @@
+#Activity 3
+import keyword
+
+# print all the Python keywords
+print("Python keywords are...\n")
+print(keyword.kwlist)
---
+++ variables.py
@@ -0,0 +1,15 @@
+#Activity 2
+# Creating different variables
+
+x = 8
+y = "Hello"
+print("x=",x)
+print("y=",y)
+
+codingal = "codingal"
+print(codingal)
+
+# User Input
+name = input("enter your name: ")
+
+print("\n", y, name, "\nwelcome to codingal")
