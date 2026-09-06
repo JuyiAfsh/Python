8af8ba07b200bdf96f8681901594c153dba35b1e
JuyiAfsh <hamidajannat25@gmail.com>
2026-09-06 19:40:49 +0200

    Python Operator

---
+++ Personal Goals Display.py
@@ -0,0 +1,11 @@
+import keyword
+
+# print all the Python keywords
+print("The keywords in python are...\n")
+print(keyword.kwlist)
+
+# Questions
+name = input("enter your name: ")
+goal = input("enter one thing you want to become or be better at")
+month = input("enter the month you would like to reach this goal")
+
